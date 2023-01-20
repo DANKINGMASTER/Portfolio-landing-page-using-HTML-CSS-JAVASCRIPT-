a8619c2f231ea15eb3948c9b344fca0dea2658aa
Vivek Pal <121789596+DANKINGMASTER@users.noreply.github.com>
2023-01-20 23:56:54 +0530

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,12 @@
+
+// Typing animation effect
+var typed = new Typed(".effect",{
+    strings:[
+        "Full Stack Developer"," MERN Developer",
+        "Programmer", "Freshers",
+        "Freelancer"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+});
